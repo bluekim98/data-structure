c78b93e3e572f1dc71200bc70540219be13b9917
bluekim <ohan09@naver.com>
2021-04-03 01:07:18 +0900

    array

---
+++ src/search/BinarySearch.java
@@ -0,0 +1,30 @@
+package search;
+
+public class BinarySearch {
+    public static int search(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left <= right) {
+            int pivot = (left + right) / 2;
+            if(target == arr[pivot]) {
+                return pivot;
+            } else if(target < arr[pivot]) {
+                right = pivot - 1;
+            } else if(target > arr[pivot]) {
+                left = pivot + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 6, 10, 15, 19};
+        int target = 5;
+
+        int targetIndex = BinarySearch.search(arr, target);
+
+        System.out.println(arr[targetIndex] == target);
+    }
+
+}
---
+++ src/sort/QuickSort.java
@@ -0,0 +1,50 @@
+package sort;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void sort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort(int[] arr, int start, int end) {
+        int partIdx = partition(arr, start, end);
+        if(start < partIdx - 1) {
+            quickSort(arr, start, partIdx - 1);
+        }
+        if(end > partIdx) {
+            quickSort(arr, partIdx, end);
+        }
+    }
+
+    private static int partition(int[] arr, int start, int end) {
+        int middleIndex = (start + end) / 2;
+        int pivot = arr[middleIndex];
+
+        while(start <= end) {
+            while(arr[start] < pivot) start++;
+            while(arr[end] > pivot) end--;
+
+            if(start <= end) {
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
+
+    private static void swap(int[] arr, int start, int end) {
+        int tmp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = tmp;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int[] arr = {4, 2, 1, 6, 10, 17, 10, 3};
+        QuickSort.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
